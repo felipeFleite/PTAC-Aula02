f5dd6d2732ba2e0ba921b9d606c7388ee79ba6ca
felipeFleite <felipefracalossileite@gmail.com>
2024-03-14 19:50:33 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Footer from "./components/footer"
-import Header from "./components/header"
+import Footer from "./Components/footer"
+import Header from "./Components/Header"
 
 import "./globals.css"
 
